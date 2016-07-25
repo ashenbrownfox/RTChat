b9cc5d93be0d64a7c11aa244f0fefbad2410224a
ashenbrownfox <newhoopsdynasty@gmail.com>
2016-07-24 20:37:31 -0400

    Establishing a connection to NodeJS. And mongo

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-console.log("It works!")
+var mongo = require('mongodb').MongoClient,
+	client = require('socket.io').listen(8080).sockets;
+
+mongo.connect('monogodb://127.0.0.1/chat',function(err,db){
+	if(err) throw err;
+
+	client.on('connection',function(socket){
+
+		var col = db.collection('messages');
+		//wait for input
+		socket.on('input',function(data){
+			var name = data.name,
+				message = data.message,
+				whitespacePattern = /^\s*$/ ;
+			if(whitespacePattern.test(name)|| whitespacePattern.test(message)){
+				console.log("Bad Data");
+			}
+			else{
+				col.insert({name: name, message: message}, function(){
+				console.log("inserted!")
+			});
+			}
+		});
+	});
+});
